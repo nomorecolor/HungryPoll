00777d8cbcc5f9b7765d614b7e06ff2cfff2f71d
Alain Elemia <Alain.Elemia@gmail.com>
2023-12-21 21:12:07 +0800

    Add contracts project

---
+++ src/HungryPoll.Contracts/FoodVoteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HungryPoll.Contracts
+{
+	public class FoodVoteDto
+	{
+		public string FoodName { get; set; } = string.Empty;
+		public UserDto CreatedByUser { get; set; } = null!;
+		public IEnumerable<UserDto> Voters { get; set; } = Enumerable.Empty<UserDto>();
+	}
+}
