ae0a29e3a9d6bb5137f943a95fd6b8020d52db56
Alain Elemia <Alain.Elemia@gmail.com>
2023-12-21 21:13:41 +0800

    Add initial handler code

---
+++ src/HungryPoll.Handler/Mappers/UserMapper.cs
@@ -0,0 +1,18 @@
+﻿using HungryPoll.Contracts;
+using HungryPoll.Domain.Models;
+
+namespace HungryPoll.Handler.Mappers
+{
+	public static class UserMapper
+	{
+		public static UserDto ToDto(this User user)
+		{
+			return new UserDto
+			{
+				Id = user.Id,
+				Name = $"{user.FirstName} {user.LastName}",
+				ImageUrl = user.ImageUrl
+			};
+		}
+	}
+}
